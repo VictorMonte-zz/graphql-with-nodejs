4c1bb9d6c1bbc5f1b9f0bf6fd8d4b9570958a768
Victor Monte <victor.monte@wirecard.com>
2019-02-07 14:18:40 -0200

    Set query schema for app

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 const express = require('express');
+const graphqlHTTP = require('express-graphql');
+const {GraphQLSchema} = require('graphql');
+
+const {queryType} = require('./query.js');
+
 const port = 5000;
 const app = express();
 
-app.get('/hello', (req, res) => {
-        res.send("hello");
-    }
-);
+const schema = new GraphQLSchema({ query: queryType });
+
+app.use('/graphql', graphqlHTTP({
+    schema: schema,
+    graphiql: true,
+}));
 
 app.listen(port);
-console.log(`Server running at localhost: ${port}`);
+console.log(`GraphQL Server Running at localhost:${port}`);
